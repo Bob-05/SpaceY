a4176467057c38a8139ac4d297ec6e6c2bc382ce
Bob-05 <studentazizov@gmail.com>
2026-09-09 12:00:19 +0300

    Добавлен новыйй метод sayBy

--- test.js
+++ test.js
@@ -5,6 +5,13 @@ class User {
   sayHi() {
     return `Привет, ${this.name}`;
   }
+
+  sayBy() {
+    return `Пока, ${this.name}`
+  }
 }
 
 const user = new User('Иван');
+
+console.log(user.sayHi());
+console.log(user.sayBy())
